bb1b809c6753f5b751152804e81908d7a6835794
spa <spa>
2004-04-27 09:32:32 +0000

    Be more informative, esp. for repeated runs.

--- examples/bench.pl
+++ examples/bench.pl
@@ -1,20 +1,23 @@
-:- initialization(benchmark).
+:- initialization(go).
 
-benchmark :-
+go :-
+	argument_list([COUNT_, BINARY_]),
+	read_term_from_atom(COUNT_, COUNT, [end_of_term(eof)]),
+	read_term_from_atom(BINARY_, BINARY, [end_of_term(eof)]),
+	benchmark(COUNT, BINARY).
+
+benchmark(COUNT, BINARY) :-
 	statistics(user_time, _),
 	statistics(system_time, _),
 	statistics(real_time, _),
 	x_connect,
-	argument_list([COUNT_, BINARY_]),
-	read_term_from_atom(COUNT_, COUNT, [end_of_term(eof)]),
-	read_term_from_atom(BINARY_, BINARY, [end_of_term(eof)]),
 	pq_set_binary(0, BINARY),
 	for(_, 1, COUNT),
 	x_get(_),
 	fail.
-benchmark :-
+benchmark(COUNT, BINARY) :-
 	statistics(user_time, [UT,_]),
 	statistics(system_time, [ST,_]),
 	statistics(real_time, [RT,_]),
-	format("user %d, system %d, elapsed %d\n", [UT, ST, RT]),
+	format("%d\t%d\t%d\t%d\t%d\n", [COUNT, BINARY, UT, ST, RT]),
 	halt.
